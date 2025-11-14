e319110bc26921ab6742df4f5da7e5fe3fb1e4fd
rhann7 <raihandityam09@gmail.com>
2025-11-14 19:01:40 +0700

    feature: implement WorkspaceMember model

--- app/Models/WorkspaceMember.php
+++ app/Models/WorkspaceMember.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class WorkspaceMember extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['workspace_id', 'user_id', 'role'];
+
+    public function workspace() { return $this->belongsTo(Workspace::class); }
+    public function user() { return $this->belongsTo(User::class); }
 }
